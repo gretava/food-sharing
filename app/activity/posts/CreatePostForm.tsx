825178b8605a8b1f4a1969a008067fa9cda6922b
Greta Va <greta.vasilevskyte@gmail.com>
2023-07-04 15:20:56 +0200

    Add userId to body

--- app/activity/posts/CreatePostForm.tsx
+++ app/activity/posts/CreatePostForm.tsx
@@ -5,31 +5,46 @@ import { Post } from '../../../migrations/1688030244-createTablePosts';
 
 type Props = {
   posts: Post[];
+  userId: number;
 };
 
-export default function CreatePostForm({ posts }: Props) {
+export default function CreatePostForm({ userId, posts }: Props) {
   const [postList, setPostList] = useState(posts);
+  // const [userid, setUserid] = useState(post.id);
+  // const [postCreateDate, setPostCreateDate] = useState();
   const [postContent, setPostContent] = useState('');
 
   async function createPost() {
     const response = await fetch('/api/posts', {
       method: 'POST',
       body: JSON.stringify({
+        userId,
         content: postContent,
+        // createdAt: postCreateDate,
       }),
     });
 
     const data = await response.json();
-
-    setPostList([...postList, data.post]);
+    console.log(data);
+    // setPostCreateDate(data.post.createdAt);
+    // setPostList([...postList, data.post]);
   }
 
+  // postList.map((post) => {
   return (
-    <>
-      <textarea placeholder="Say something..." />
-      <button>Post</button>
-    </>
+    // <div key={`post-content-${post.id}`}>
+    <div>
+      <br />
+
+      <textarea
+        value={postContent}
+        onChange={(event) => setPostContent(event.currentTarget.value)}
+        placeholder="Say something..."
+      />
+      <button onClick={async () => await createPost()}>Post</button>
+    </div>
   );
+  // });
 }
 
 // 1. create API route to insert post
