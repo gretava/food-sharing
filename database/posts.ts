4b79383a5cfc414a088c41dc03f4f3a464b2dc7c
Greta Va <greta.vasilevskyte@gmail.com>
2023-07-04 15:23:56 +0200

    Remove unnecessary variables

--- database/posts.ts
+++ database/posts.ts
@@ -2,23 +2,22 @@ import { cache } from 'react';
 import { Post } from '../migrations/1688030244-createTablePosts';
 import { sql } from './connect';
 
-export const createPost = cache(
-  async (userId: number, content: string, session: number) => {
-    const [post] = await sql<Post[]>`
+export const createPost = cache(async (userId: number, content: string) => {
+  const [post] = await sql<Post[]>`
     INSERT INTO posts
-      (userId, content, session)
+      (user_id, content) -- deleted session
     VALUES
-      (${userId}, ${content}, ${session})
+      (${userId}, ${content}) -- deleted session
     RETURNING
       id,
       userId,
-      content,
-      session
+      content
+      -- createdAt
+      -- session
  `;
 
-    return post;
-  },
-);
+  return post;
+});
 
 export const getPostById = cache(async (id: number) => {
   const [post] = await sql<Post[]>`
@@ -33,11 +32,38 @@ export const getPostById = cache(async (id: number) => {
 });
 
 export const getAllPosts = cache(async () => {
-  const [post] = await sql<Post[]>`
+  const posts = await sql<Post[]>`
     SELECT
       *
     FROM
       posts
   `;
-  return post;
+  return posts;
 });
+
+export const getPostsWithLimitAndOffsetBySessionToken = cache(
+  async (limit: number, offset: number, token: string) => {
+    const posts = await sql<Post[]>`
+      SELECT
+        posts.*
+      FROM
+        posts
+      INNER JOIN
+        sessions ON (
+          sessions.token = ${token} AND
+          sessions.expiry_timestamp > now()
+          sessions.user_id = posts.user_id
+        )
+      -- This would JOIN the users table that is related to animals
+      -- INNER JOIN
+      --   users ON (
+      --     users.id = animals.user_id AND
+      --     sessions.user_id = users.id
+      --   )
+      LIMIT ${limit}
+      OFFSET ${offset}
+    `;
+
+    return posts;
+  },
+);
