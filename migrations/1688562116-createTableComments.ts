63e42a0cabab2605de456b463c485781c9a0593d
Greta Va <greta.vasilevskyte@gmail.com>
2023-07-05 15:11:25 +0200

    Create database for comments

---
+++ migrations/1688562116-createTableComments.ts
@@ -0,0 +1,27 @@
+import { Sql } from 'postgres';
+
+export type Comment = {
+  id: number;
+  post_id: number;
+  user_id: number;
+  content: string;
+  created_at: string;
+};
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE comments (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      post_id integer NOT NULL REFERENCES posts (id),
+      user_id integer NOT NULL REFERENCES users (id),
+      content varchar(600) NOT NULL,
+      created_at timestamp NOT NULL DEFAULT NOW()
+    )
+  `;
+}
+
+export async function down(sql: Sql) {
+  await sql`
+    DROP TABLE comments
+  `;
+}
