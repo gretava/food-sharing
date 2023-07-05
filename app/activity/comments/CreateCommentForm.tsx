619655b6bd4fadb7271ab7c6245f88210df89a2c
Greta Va <greta.vasilevskyte@gmail.com>
2023-07-05 23:06:53 +0200

    Add CreateCommentForm to post comments

---
+++ app/activity/comments/CreateCommentForm.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+type Props = {
+  comments: Comment[];
+  postId: number;
+  userId: number;
+};
+
+export default function CreateCommentForm({ postId, userId, comments }: Props) {
+  const [commentContent, setCommentContent] = useState('');
+  const router = useRouter();
+
+  async function createComment() {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({
+        postId,
+        userId,
+        content: commentContent,
+      }),
+    });
+    router.refresh();
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={(event) => event.preventDefault()}>
+      <div>
+        <textarea
+          value={commentContent}
+          onChange={(event) => setCommentContent(event.currentTarget.value)}
+          placeholder="Reply"
+        />
+        <button onClick={async () => await createComment()}>Reply</button>
+      </div>
+    </form>
+  );
+}
