0a018c1cfa93bd945a9ea9aa33537515fc660a1e
Greta Va <greta.vasilevskyte@gmail.com>
2023-07-05 10:12:51 +0200

    Add router.refresh and fn to empty the input field

--- app/activity/posts/CreatePostForm.tsx
+++ app/activity/posts/CreatePostForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Post } from '../../../migrations/1688030244-createTablePosts';
 
@@ -11,8 +12,9 @@ type Props = {
 export default function CreatePostForm({ userId, posts }: Props) {
   const [postList, setPostList] = useState(posts);
   // const [userid, setUserid] = useState(post.id);
-  // const [postCreateDate, setPostCreateDate] = useState();
   const [postContent, setPostContent] = useState('');
+
+  const router = useRouter();
 
   async function createPost() {
     const response = await fetch('/api/posts', {
@@ -20,33 +22,28 @@ export default function CreatePostForm({ userId, posts }: Props) {
       body: JSON.stringify({
         userId,
         content: postContent,
-        // createdAt: postCreateDate,
       }),
     });
+    router.refresh();
 
     const data = await response.json();
     console.log(data);
-    // setPostCreateDate(data.post.createdAt);
     // setPostList([...postList, data.post]);
+
+    setPostContent('');
   }
 
-  // postList.map((post) => {
   return (
-    // <div key={`post-content-${post.id}`}>
-    <div>
-      <br />
-
-      <textarea
-        value={postContent}
-        onChange={(event) => setPostContent(event.currentTarget.value)}
-        placeholder="Say something..."
-      />
-      <button onClick={async () => await createPost()}>Post</button>
-    </div>
+    <form onSubmit={(event) => event.preventDefault()}>
+      <div>
+        <br />
+        <textarea
+          value={postContent}
+          onChange={(event) => setPostContent(event.currentTarget.value)}
+          placeholder="Say something..."
+        />
+        <button onClick={async () => await createPost()}>Post</button>
+      </div>
+    </form>
   );
-  // });
 }
-
-// 1. create API route to insert post
-// 2. get body from textarea and user id from session cookie
-// 3. call a function to insert the data into database
